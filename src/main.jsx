c4e86d8d891161d787dbc5ebd7ec85d352782b6b
Thanaphat Tangcham <tangcham.thanaphat@gmail.com>
2025-12-18 14:34:54 +0700

    fix: color4bg

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import 'seedrandom';
-Math.seedrandom = seedrandom
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+if (window.seedrandom) {
+  Math.seedrandom = window.seedrandom;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
